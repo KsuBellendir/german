91c37fa34e219ef2999bf2037e2d5a0ead794863
Mama <KsuNami@yandex.ru>
2022-05-13 15:22:08 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use sctrict';
+const answer = [];
+answer[0] = prompt('как ваше имя?', "");
+answer[1] = prompt('как ваше фамилия?', "");
+answer[2] = prompt('сколько вам лет?', "");
+
+document.write(answer);
